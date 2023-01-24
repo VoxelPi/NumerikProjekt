7c2871b89bd4eafde6cba45103caf1f0eb996d36
Binom <45207156+bodokat@users.noreply.github.com>
2023-01-24 16:46:26 +0100

    tests

--- 3d.py
+++ 3d.py
@@ -15,7 +15,7 @@ def quadTriangle(n, z):
     for x_i in range(n):
         for y_i in range(n):
             v = psi(z, phi((x_1d[x_i]+1)*0.5, (x_1d[y_i]+1)*0.5))
-            w = w_1d[x_i]*w_1d[y_i]*(1-v[0])*np.linalg.det(A(z))*0.25
+            w = w_1d[x_i]*w_1d[y_i]*(1-(x_1d[x_i]+1)*0.5)*abs(np.linalg.det(A(z)))*0.25
             x_2d[x_i + y_i * n] = v
             w_2d[x_i + y_i * n] = w
 
@@ -29,6 +29,36 @@ def psi(z, v):
 
 z = np.array([[0, 0], [1, 0], [0, 1]])
 
+def runQuad(quad,f):
+    xs,ws = quad
+    return sum([f(x)*w for (x,w) in zip(xs,ws)])
+
+def test_poly(p,quad):
+    expected = sum([a*(1/(l+1))*sum([binom(l+1,i)*(-1)**i*(1/(k+i+1)) for i in range(l+2)]) for (k,p1) in enumerate(p) for (l,a) in enumerate(p1)])
+    def f(v):
+        return sum([a*v[0]**k*v[1]**l for (k,p1) in enumerate(p) for (l,a) in enumerate(p1)])
+    q = runQuad(quad,f)
+    return expected - q
+
+p = [[2,4,3],[7,1],[3]]
+test_poly(p,quadTriangle(4,z))
+
+def f(v):
+    return np.exp(v[0]+v[1])
+def g(v):
+    return np.sqrt(v[0]+v[1])
+
+expected = 1
+for n in range(1,20):
+    quad = quadTriangle(n,z)
+    q = runQuad(quad, f)
+    print("n: ",n,", q: ",q,", diff:", q-expected)
+
+expected = 6/15
+for n in range(1,40):
+    quad = quadTriangle(n,z)
+    q = runQuad(quad, g)
+    print("n: ",n,", q: ",q,", diff:", q-expected)
 # print(A(z))
 # print(A(z)@np.array([0.5, 0.5]))
 
