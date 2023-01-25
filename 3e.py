f1cad47cf69bc5b261aebb6dd85db3bd6201707b
VoxelPi <smek.peter@gmail.com>
2023-01-25 16:38:59 +0100

    Create error plot

--- 3e.py
+++ 3e.py
@@ -3,28 +3,36 @@ import matplotlib.pyplot as plt
 
 from quad2d import quadTriangle
 
+# Define reference triangle.
 z = np.array([[0, 0], [1, 0], [0, 1]])
 
-# def f(x):
-#     return np.exp(x[0] + x[1])
+# Approximate the integral.
+def approximate(f, n):
+    xs, ws = quadTriangle(n, z)
+    return sum([f(x)*w for (x,w) in zip(xs,ws)])
 
-def f(x):
-    return np.sqrt(x[0] + x[1])
+# Plot the error
+def plot_error(n, f, expected, label):
+    approx = np.array([approximate(f, i) for i in n])
 
-def approximate(n):
-    (x, w) = quadTriangle(n, z)
-    sum = 0
-    for i in range(n**2):
-        sum += w[i] * f(x[i])
-    return sum
+    plt.plot(n**2, approx - expected, label=label)
 
-n = np.arange(1, 11, 1)
-approx = np.vectorize(approximate)(n)
-
+# Create the plot.
+n = np.arange(1, 5+1, 1)
 plt.figure()
-plt.plot(n, approx)
-plt.xlabel("n")
-plt.ylabel("approximation")
-plt.title("$f(x,y) = \sqrt{x + y}$")
+
+f_expected = 1
+f = lambda x : np.exp(x[0] + x[1])
+plot_error(n, f, f_expected, "$\exp(x + y)$")
+
+g_expected = 6/15
+g = lambda x : np.sqrt(x[0] + x[1])
+plot_error(n, g, g_expected, "$\sqrt{x + y}$")
+
+plt.xlabel("Anzahl der Funktionsauswertungen n")
+plt.xticks(n**2)
+plt.ylabel("Quadraturfehler")
+plt.title("Quadraturfehler")
+plt.legend()
 plt.grid()
 plt.show()
